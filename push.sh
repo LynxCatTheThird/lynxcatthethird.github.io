567d1b7b7bbea24daa56f0f2a53ce033ebf159f3
LynxCatTheThird <tsfxlyx@qq.com>
2025-02-20 00:15:23 +0800

    Site updated: 2025-02-20 00:15:23

--- push.sh
+++ push.sh
@@ -2,7 +2,12 @@ cd /home/lynx/Program/web-v3/source/
 mv /home/lynx/Program/web-v3/source/_cached_git_source /home/lynx/Program/web-v3/source/.git
 mv /home/lynx/Program/web-v3/.git /home/lynx/Program/web-v3/_cached_git_parent
 git add .
-git commit -m "Sources updated: $(date '+%Y-%m-%d %H:%M:%S')"
+# 获取修改的文件名并按行分开
+modified_files=$(git status --porcelain | awk '{print $2}' | tr '\n' '\n')
+# 使用 printf 或 echo 来确保多行提交信息
+commit_msg="Sources updated: $(date '+%Y-%m-%d %H:%M:%S')\nModified files:\n$modified_files"
+# 提交并推送
+git commit -m "$(echo -e "$commit_msg")"
 git push
 mv /home/lynx/Program/web-v3/source/.git /home/lynx/Program/web-v3/source/_cached_git_source
 mv /home/lynx/Program/web-v3/_cached_git_parent /home/lynx/Program/web-v3/.git
