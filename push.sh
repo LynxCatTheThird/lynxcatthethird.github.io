8b2bc2d9984b50870c301fc52996e0937bdaa83a
LynxCatTheThird <contact@lynx3.me>
2026-05-01 11:39:50 +0800

    Site updated: 2026-05-01 11:39:50

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-cd /home/lynx/Program/web-v3/source/
-mv /home/lynx/Program/web-v3/source/_cached_git_source /home/lynx/Program/web-v3/source/.git
-mv /home/lynx/Program/web-v3/.git /home/lynx/Program/web-v3/_cached_git_parent
+cd /home/lynx/Services/web-v3/source/
+mv /home/lynx/Services/web-v3/source/_cached_git_source /home/lynx/Services/web-v3/source/.git
+mv /home/lynx/Services/web-v3/.git /home/lynx/Services/web-v3/_cached_git_parent
 git add .
 # 获取修改的文件名并按行分开
 modified_files=$(git status --porcelain | awk '{print $2}' | tr '\n' '\n')
@@ -9,5 +9,5 @@ commit_msg="Sources updated: $(date '+%Y-%m-%d %H:%M:%S')\nModified files:\n$mod
 # 提交并推送
 git commit -m "$(echo -e "$commit_msg")"
 git push
-mv /home/lynx/Program/web-v3/source/.git /home/lynx/Program/web-v3/source/_cached_git_source
-mv /home/lynx/Program/web-v3/_cached_git_parent /home/lynx/Program/web-v3/.git
+mv /home/lynx/Services/web-v3/source/.git /home/lynx/Services/web-v3/source/_cached_git_source
+mv /home/lynx/Services/web-v3/_cached_git_parent /home/lynx/Services/web-v3/.git
